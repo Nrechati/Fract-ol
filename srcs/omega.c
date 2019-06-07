a3cfdb7cc45dd194ac86e5ee14fe01fb7234fc81
Hyphi <nrechati@student.42.fr>
2019-06-07 10:23:48 +0200

    [NEW FRAC] OMEGA

--- srcs/omega.c
+++ srcs/omega.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   mandelbrot.c                                       :+:      :+:    :+:   */
+/*   omega.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nrechati <nrechati@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/14 10:22:17 by nrechati          #+#    #+#             */
-/*   Updated: 2019/06/07 10:23:33 by nrechati         ###   ########.fr       */
+/*   Created: 2019/06/07 10:14:07 by nrechati          #+#    #+#             */
+/*   Updated: 2019/06/07 10:23:09 by nrechati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-static int		iterate_mandelbrot(t_mlx *mlx, t_comp c)
+static int		iterate_omega(t_mlx *mlx, t_comp c)
 {
 	int		i;
 	t_comp	z;
@@ -26,11 +26,12 @@ static int		iterate_mandelbrot(t_mlx *mlx, t_comp c)
 	im_sq = 0;
 	while (i <= mlx->iter && re_sq + im_sq <= 4)
 	{
-		re_sq = z.re * z.re;
-		im_sq = z.im * z.im;
-		z.im = (z.re + z.im) * (z.re + z.im) - re_sq - im_sq;
-		z.im += c.im;
-		z.re = re_sq - im_sq + c.re;
+		re_sq = c.re * c.re;
+		im_sq = c.im * c.im;
+		c.im = 2 * c.re * c.im)
+		c.re = re_sq - im_sq;
+		z.re = sinh(z.re) + (1 / c.re);
+		z.im = sinh(z.im) + (1 / c.im);
 		i++;
 	}
 	if (i <= mlx->iter)
@@ -38,7 +39,7 @@ static int		iterate_mandelbrot(t_mlx *mlx, t_comp c)
 	return (FAILURE);
 }
 
-static void		compute_line_mandelbrot(t_thread *thd, int y, t_comp c)
+static void		compute_line_omega(t_thread *thd, int y, t_comp c)
 {
 	int		x;
 	int		iter;
@@ -49,13 +50,13 @@ static void		compute_line_mandelbrot(t_thread *thd, int y, t_comp c)
 	{
 		c.re = 2 * (x - thd->mlx->w2)
 				/ (thd->mlx->zoom * thd->mlx->w2) - thd->mlx->x_pad;
-		if ((iter = iterate_mandelbrot(thd->mlx, c)) != FAILURE)
+		if ((iter = iterate_omega(thd->mlx, c)) != FAILURE)
 			fill_pixel(thd->mlx, x, y, get_color(thd->mlx, iter));
 		x++;
 	}
 }
 
-void			*is_mandelbrot(void *arg)
+void			*is_omega(void *arg)
 {
 	int			max;
 	int			y;
@@ -69,7 +70,7 @@ void			*is_mandelbrot(void *arg)
 	{
 		c.im = (y - thd->mlx->h2)
 				/ (thd->mlx->zoom * thd->mlx->h2) + thd->mlx->y_pad;
-		compute_line_mandelbrot(thd, y, c);
+		compute_line_omega(thd, y, c);
 		y++;
 	}
 	return (NULL);
